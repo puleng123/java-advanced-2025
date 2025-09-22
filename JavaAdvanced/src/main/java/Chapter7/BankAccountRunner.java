d307e12d9fced7d259d5b4017067f3010535884f
puleng123 <Puleng.Senyatso@ilabqa.com>
2025-09-22 13:59:59 +0200

    Closing student class, close #1

---
+++ JavaAdvanced/src/main/java/Chapter7/BankAccountRunner.java
@@ -0,0 +1,24 @@
+package Chapter7;
+
+public class BankAccountRunner
+{
+    public static void main(String[] args)
+    {
+        BankAccount objBank = new BankAccount("Joe Smith", "123456789", "Debit", 5000.89);
+        objBank.displayDetails();
+
+        objBank.setBalance(15000.00);
+        objBank.setAccHolder("");
+        objBank.deposit(500);
+        objBank.withdraw(200);
+        objBank.transfer(100);
+
+        System.out.println("=========================================");
+        objBank.displayDetails();
+
+
+        System.out.println("Current balance for (" + objBank.getAccHolder() +
+                " is R" + objBank.getBalance());
+
+    }
+}
